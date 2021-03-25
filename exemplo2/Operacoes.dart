92146aa245cc810f2280eee2dffb7a69a44f10b0
Wesley <kwesley15@hotmail.com>
2021-03-25 00:09:00 +0000

    otherthing

---
+++ exemplo2/Operacoes.dart
@@ -0,0 +1,20 @@
+import Math
+
+class Operacoes {
+  dynamic somar(var v1, var v2) {
+    return v1 + v2;
+  }
+
+  dynamic multiplicar(var v1, var v2) {
+    return v1 * v2;
+  }
+
+  dynamic potencia(var base, var expoente){
+    return pow(base,expoente);
+
+  bool isPar(var valor){
+    return (valor % 2 == 0)
+    }
+  }
+
+}
---
+++ exemplo2/main.dart
@@ -0,0 +1,11 @@
+void main(List<String> args){
+  var op = Operacoes();
+  var v1 = 2
+  var v2 = 3
+
+  print(op.somar(v1, v2));
+  print(op.somar(v1, v2));
+  print(op.somar(v1, v2));
+  print(op.somar(v1, v2));
+  print(op.somar(v1, v2));
+  
